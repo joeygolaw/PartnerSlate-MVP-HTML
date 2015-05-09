6bd8f3ecefdab292008bbf0eb167995c0a960797
Joey Golaw <look@joeygolaw.com>
2015-05-09 09:10:11 -0700

    Updated Landing Page
    - Fixed responsive design
    - Updated header menu functionality
    - Added demo Start Searching overlay panel
    - Minor revisions to style
    - Re-organized SASS files back to structure from initial asset delivery
    for MVP

--- js/default.js
+++ js/default.js
@@ -14,29 +14,8 @@ $(".clear-all-closable").click(function() {
 	$(this).remove();
 	});
 
-/* Page Topbar, User Menu */
-$(".ps-user-menu").click(function(){ 
-	$(this).toggleClass("toggled");
-	return false;
-	});
-/* Custom Dropdown Input */
-$(".dropdown-field").click(function(){ 
-	$(this).toggleClass("toggled").children(".dropdown-options");
-	});
 
-/* Star icon toggle */
-$(".fa.fa-star-o").click(function(){
-	$(this).toggleClass("fa-star-o").toggleClass("fa-star").toggleClass("toggled");
-	});
-$(".fa.fa-star").click(function(){
-	$(this).toggleClass("fa-star").toggleClass("fa-star-o").toggleClass("toggled");
-	});
-
-/*Anchor Smooth-Scroll*/
-$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var target=$(this.hash);target=target.length?target:$("[name="+this.hash.slice(1)+"]");if(target.length){$("html,body").animate({scrollTop:target.offset().top},480);return false;}}});});
-
-
-/* Page Overlay */
+// ---------- Page Overlay
 // Define Variables
 var $pageOverlay = $(".ps-page-overlay");
 var $popoverPanel = $(".ps-popover", $pageOverlay);
@@ -63,3 +42,44 @@ function hideOverlay() {
 	$pageOverlay.css({"z-index": "-1", "opacity": "0"});
 	};
 $pageDimmer.on('click', hideOverlay);
+
+
+// ---------- Page Topbar
+// User Menu
+$(".ps-user-menu").click(function(){ 
+	$(this).toggleClass("toggled");
+	return false;
+	});
+// Sticky Nav
+$(window).scroll(function() {
+	var offset = 0;
+	var sticky = false;
+	var topWindow = $(window).scrollTop();
+	var $pageHeader = $("#ps-page-header");
+
+	if ($(".slider-message").offset().top < topWindow) {
+		$pageHeader.addClass("fixed");
+		sticky = true;
+		}
+	else {
+		$pageHeader.removeClass("fixed");
+		}
+		});
+
+
+// ---------- Miscellaneous
+// Custom Dropdown Input
+$(".dropdown-field").click(function(){ 
+	$(this).toggleClass("toggled").children(".dropdown-options");
+	});
+
+// Star Icon Toggle
+$(".fa.fa-star-o").click(function(){
+	$(this).toggleClass("fa-star-o").toggleClass("fa-star").toggleClass("toggled");
+	});
+$(".fa.fa-star").click(function(){
+	$(this).toggleClass("fa-star").toggleClass("fa-star-o").toggleClass("toggled");
+	});
+
+// Anchor Smooth-Scroll
+$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var target=$(this.hash);target=target.length?target:$("[name="+this.hash.slice(1)+"]");if(target.length){$("html,body").animate({scrollTop:target.offset().top},480);return false;}}});});
